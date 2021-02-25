56392bb3e83c1e41c7fa157f241595cc3373ce04
John Reiley <supermormonbrother@gmail.com>
2021-02-24 22:18:02 -0700

    added join functionality to Join page and removed build folder from source

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,29 +1,71 @@
+import { useState } from 'react';
 import './Join.css';
+import Socket from '../../services/SocketService';
 
 function Join() {
+  const [partyCode, setPartyCode] = useState();
+  const [name, setName] = useState();
+  const [showCodeWarning, setShowCodeWarning] = useState();
+  const [showNameWarning, setShowNameWarning] = useState();
 
   const onSubmit = (e) => {
     e.preventDefault();
+    let valid = true;
+    if (partyCode.length < 4) {
+      setShowCodeWarning(true);
+      valid = false;
+    }
+    if (name.length < 1) {
+      setShowNameWarning(true);
+      valid = false;
+    }
+    if (valid) {
+      Socket.emit('joinParty', { name, partyId: partyCode })
+    }
   }
 
   return (
     <>
       <div className="row">
         <div className="Join-header">
-          <h1 className="theme-font text-light text-uppercase">Studygorize Party</h1>
+          <h1 className="theme-font text-light">Studygorize Party</h1>
         </div>
       </div>
       <div className="row">
         <form onSubmit={onSubmit} id="joinForm">
           <div className="form-group">
-            <label htmlFor="roomCodeInput" className="text-uppercase text-light">Room Code</label>
-            <input type="text" id="roomCodeInput" className="form-control" maxLength="4" placeholder="enter 4-letter code" />
+            <label htmlFor="partyCodeInput" className="text-light">Party Code</label>
+            <input onChange={e => setPartyCode(e.target.value)} 
+              type="text" 
+              id="partyCodeInput" 
+              className="form-control text-uppercase" 
+              maxLength="4" 
+              placeholder="enter 4-letter code"
+              required
+              />
+            {showCodeWarning &&
+              <div className="alert alert-warning mt-1">
+                The code needs to be 4 letters long
+              </div>
+            }
           </div>
           <div className="form-group">
-            <label htmlFor="nameInput" className="text-uppercase text-light">Name</label>
-            <input type="text" id="nameInput" className="form-control" maxLength="12" placeholder="enter your name" />
+            <label htmlFor="nameInput" className="text-light">Name</label>
+            <input onChange={e => setName(e.target.value)} 
+              type="text" 
+              id="nameInput" 
+              className="form-control" 
+              maxLength="12" 
+              placeholder="enter your name"
+              required
+              />
+            {showNameWarning &&
+              <div className="alert alert-warning mt-1">
+                Enter your name
+              </div>
+            }
           </div>
-          <button type="submit" id="joinBtn" className="btn btn-light text-uppercase">Join</button>
+          <button type="submit" id="joinBtn" className="btn btn-light">Join</button>
         </form>
       </div>
       <div className="row">
